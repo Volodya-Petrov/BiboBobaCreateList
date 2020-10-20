5c56a5cdb25f3e09f76c33524306a2039806a025
StenDarr <89377379140@mail.ru>
2020-10-20 14:33:07 +0300

    Task3

--- List/List/List.c
+++ List/List/List.c
@@ -1,6 +1,46 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	int number;
+	ListElement *next;
+} ListElement;
+
+typedef struct
+{
+	ListElement* head;
+}List;
+
+ListElement* insert(ListElement* head, int index)
+{
+	ListElement* currentElement = head;
+	for (int i = 0; i < index; ++i)
+	{
+		currentElement = currentElement->next;
+	}
+	ListElement* newItem = malloc(sizeof(ListElement));
+	newItem = 
+	
+}
+
+List* createList(int numberOfElements)
+{	
+	ListElement* head = malloc(sizeof(ListElement));
+	tail->number = 0;
+	tail->next = NULL;
+	List* newList = malloc(sizeof(List));
+	newList->head = &head;
+ 	for (int i = 0; i < numberOfElements - 1; i++)
+	{
+		newList->header = append();
+	}
+}
+
+
 
 int main(void)
 {
-	printf("лОХ");
+	ListElement* head = NULL;
+
 }
---
+++ Test1.3/Test1.3/Test1.3.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int main(void)
+{	
+	setlocale(LC_ALL, "");
+	FILE* fileOpen = fopen("test.txt", "r");
+	char charInReal = '\0';
+	bool beginOfString = true;
+	bool needPrint = false;
+	while ((charInReal = fgetc(fileOpen)) != EOF)
+	{
+		if (beginOfString)
+		{
+			if (charInReal == ';')
+			{
+				needPrint = true;
+				beginOfString = false;
+			}
+		}
+		if (needPrint)
+		{
+			printf("%c", charInReal);
+		}
+		if (charInReal == '\n')
+		{
+			beginOfString = true;
+			needPrint = false;
+		}
+	}
+	fclose(fileOpen);
+}
