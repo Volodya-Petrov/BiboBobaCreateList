0f7991c5bbc6494484ba3dcc15f2502ae34367ea
StenDarr <89377379140@mail.ru>
2020-11-03 14:29:17 +0300

    first com

---
+++ test1.3.3/test1.3.3/test1.3.3.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+	FILE* fileOpen = fopen("test.txt", "r");
+	char currentChar = '\0';
+	char oldChar = '\0';
+	bool firstRead = true;
+	while ((currentChar = fgetc(fileOpen)) != EOF)
+	{	
+		if (!firstRead)
+		{
+			if (currentChar != oldChar)
+			{
+				printf("%c", oldChar);
+			}
+		}
+		firstRead = false;
+		oldChar = currentChar;
+	}
+	printf("%c", oldChar);
+	fclose(fileOpen);
+}
