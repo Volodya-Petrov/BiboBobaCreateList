b9b13cdfd2081b1142fd6a9e3e759642202f12fb
StenDarr <89377379140@mail.ru>
2020-11-03 14:45:15 +0300

    ggg

--- test1.3.2/test1.3.2/test1.3.2.c
+++ test1.3.2/test1.3.2/test1.3.2.c
@@ -14,7 +14,11 @@ void printArray(int array[], int size)
 }
 
 void combSort(int array[], int size)
-{
+{   
+    if (size <= 0)
+    {
+        return;
+    }
     int gap = size;
     int swaps = 1;
     int j = 0;
@@ -45,8 +49,18 @@ int main(void)
     setlocale(LC_ALL, "rus");
     int size = 0;
     printf("Введите размер массива: ");
-    scanf("%i", &size);
+    while (scanf("%i", &size) == 0)
+    {
+        scanf("%*s");
+        printf("Некорректный ввод, повтори попытку\n");
+        printf("Введите размер массива: ");
+    }
     int* array = malloc(sizeof(int) * size);
+    if (array == NULL)
+    {
+        printf("Не дали память :(\n");
+        return 1;
+    }
     for (int i = 0; i < size; i++)
     {
         array[i] = 200 * rand() / RAND_MAX - 100;
