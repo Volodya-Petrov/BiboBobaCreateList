dc7203658e04d23b7d1a82da600751bef4c8ec52
StenDarr <89377379140@mail.ru>
2020-12-08 15:16:37 +0300

    tests, late for 1 min sorry

--- Test2Task2/Test2Task2/testForTask.c
+++ Test2Task2/Test2Task2/testForTask.c
@@ -1,2 +1,17 @@
 #include "../../NewList/List/List.h"
 #include "readFromFile.h"
+#include "transformList.h"
+#include <stdbool.h>
+
+bool testForTask(void)
+{
+	bool result = true;
+	List* list = readFromFile("test.txt");
+	list = removeDuplicates(list);
+	if (indexOf(list, "f") == -1 || indexOf(list, "abc") == -1 || indexOf(list, "ff") == 1 || length(list) != 3)
+	{
+		result = false;
+	}
+	deleteList(&list);
+	return result;
+}
