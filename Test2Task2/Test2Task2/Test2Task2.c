a9f022374f99d684edc7b254ef5171f38a8f1662
StenDarr <89377379140@mail.ru>
2020-12-08 15:01:03 +0300

    console output

--- Test2Task2/Test2Task2/Test2Task2.c
+++ Test2Task2/Test2Task2/Test2Task2.c
@@ -2,10 +2,15 @@
 #include "../../NewList/List/List.h"
 #include "transformList.h"
 #include "../../NewList/List/listTests.h"
+#include <stdio.h>
 
 int main(void)
 {	
 	List* list = readFromFile("task.txt");
 	list = removeDuplicates(list);
+	for (int i = 0; i < length(list); i++)
+	{
+		printf("%s\n", getValue(list, i));
+	}
 	deleteList(&list);
 }
